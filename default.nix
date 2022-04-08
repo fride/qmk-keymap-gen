e0877623cab342fd7e30f340714e5ac42f7180db
janfriderici <jan@friderici.net>
2022-04-08 14:46:02 +0200

    use nightly rust.

--- default.nix
+++ default.nix
@@ -1,6 +1,19 @@
 let
-    pkgs = import <nixpkgs> {};
+    # pkgs = import <nixpkgs> {};
     sources = import ./nix/sources.nix;
+    nixpkgs-mozilla = import sources.nixpkgs-mozilla;
+    pkgs = import <nixpkgs> {
+      overlays =
+      [
+        nixpkgs-mozilla
+        (self: super:
+            {
+              rustc = self.latest.rustChannels.nightly.rust;
+              cargo = self.latest.rustChannels.nightly.rust;
+            }
+        )
+      ];
+    };
     naersk = pkgs.callPackage sources.naersk {};
 in 
     naersk.buildPackage {
